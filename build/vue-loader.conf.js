f03e521cf96a1e9f7986c8316bb12747e94e891d
Gguoxiang@yeah.net <Gguoxiang@yeah.net>
2022-05-11 10:23:28 +0800

    git添加子模块

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -5,11 +5,18 @@ const isProduction = process.env.NODE_ENV === 'production'
 const sourceMapEnabled = isProduction
   ? config.build.productionSourceMap
   : config.dev.cssSourceMap
-
+  var path = require('path')
 module.exports = {
   loaders: utils.cssLoaders({
     sourceMap: sourceMapEnabled,
-    extract: isProduction
+    extract: isProduction,
+    less: ['vue-style-loader', 'css-loader', 'less-loader', {
+      loader: 'sass-resources-loader',
+      options: {resources: [
+        path.resolve(__dirname, './src/common_style/yunwen_themes.less'),
+        path.resolve(__dirname, './src/common_style/yunwen_style.less'),
+      ]}
+    }]
   }),
   cssSourceMap: sourceMapEnabled,
   cacheBusting: config.dev.cacheBusting,
--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import * as antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.less'
+import '../src/common_style/index.less'
+import YIcon from './common_style/yIcon.vue'
+Vue.component(YIcon.name,YIcon)
+
+Vue.use(antd)
+const req=require.context('./common_style/icons',false,/\.svg$/)
+console.log('req',req,req.keys())
+req.keys().map(req);// keys() 会获取所有svg 文件
 
 Vue.config.productionTip = false
 
